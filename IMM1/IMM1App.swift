46e752b6a50ae4b75c4185bd6cd19b125d1135a7
yanyanyanling <s11014069@gm.cyut.edu.tw>
2024-02-22 15:05:46 +0800

    Initial Commit

---
+++ IMM1/IMM1App.swift
@@ -0,0 +1,17 @@
+//
+//  IMM1App.swift
+//  IMM1
+//
+//  Created by Mac on 2024/2/22.
+//
+
+import SwiftUI
+
+@main
+struct IMM1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
